1e807786f119ca83648085a74fe68c89f16adae2
morning120429 <forever120429@gmail.com>
2024-02-29 18:14:52 +1000

    change name

--- total/merge_excel.py
+++ total/merge_excel.py
@@ -1,12 +1,13 @@
 import pandas as pd
 import os
+
 merged_data = pd.DataFrame()
 directory = 'excel'
 for index, filename in enumerate(os.listdir(directory)):
     if filename.endswith(".xlsx") or filename.endswith(".xls"):  # Filter only Excel files
         filepath = os.path.join(directory, filename)
         df = pd.read_excel(filepath)  # Read each Excel file into a DataFrame
-        merged_data = pd.concat([merged_data, df], ignore_index=True)
+        merged_data = pd.concat([merged_data, df], ignore_index = True)
         print(index + 1)
 
-merged_data.to_excel('merge.xlsx', index=False)
+merged_data.to_excel('merge.xlsx', index = False)
